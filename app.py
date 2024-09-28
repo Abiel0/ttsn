317857544eb36048671cb76f41ad9971dadd0ba2
Abiel0 <162965717+Abiel0@users.noreply.github.com>
2024-09-28 17:15:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def text_to_speech():
     return send_file(output_file, mimetype="audio/wav")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True)
